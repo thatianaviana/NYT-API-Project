404264fbc74c8ec2cc860c22140f9cbdff54e016
Vlad N <vlad.neri@gmail.com>
2019-04-18 16:59:12 -0700

    draft

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,64 @@
 // $(document).ready(function(){});
 //  Question for Omar - can you explain why for some of our excercises $(document).ready(function(){} were at the top
 // but for all these API activities, they are everywhere in the page? 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + keyAPI + "&q=";
-var keyAPI = "63ba1dba-02a5-4884-9453-68ae543878b6";
-var articleCounter = 0;
-var startYear = 0;
-var endYear = 0;
 
 
-function startQuery(){
-    $.ajax({
-        url: queryURL,
-        method: "GET",
+// $("#buttons-view").on("click", "button",function() {
 
-    }).then(function (response) {
-        console.log(response);
+    var apiKey = "UDp81ZvSBZ7G1huAhXF0aVboCxXxVxt6"; 
+
+    var searchTerm = "";
+    var numRecords = 0;
+    var startYear = 0;
+    var endYear = 0;
+
+    var articleCounter = 0;
+
+    //API URL 
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=&api-key=" + apiKey;
+
+
+    function runQuery(queryURL) {
+
+    $.ajax({ url: queryURL, method: "GET"})
+        .done(function(data) {
+        
+        var results = response.data; 
+           
+
+        //clears content area
+        $('#contentArea').empty();
+
+            for (var i = 0; i < queryURL; i++) {
+                console.log(response.docs[i].section_name);
+            
+            //send information to HTML
+
+            var c = $('<div>');
+            c.addClass('area');
+            c.attr('id', queryURL[i]);
+            
+            //Appends to Content Area
+            $('#contentArea').append(c);
+
+            }});
+
+        }
+
+    $('#searchBtn').on('click', function() {
+
+        searchTerm = $('#search').val().trim();
+        newURL = queryURL + "&q=" + searchTerm; 
+        numRecords = $('#numRecords').val();
+        startYear = $('#startYear').val().trim();
+        endYear = $('#endYear').val().trim();
 
     });
-}
-startQuery();    //getting error in console 401 unauthorized
+
+
+
+
+      
+
 
 
