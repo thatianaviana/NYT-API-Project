90b61e6dff71f39b58167c811f5d344968a75391
Thatiana <cruz.thatiana@gmail.com>
2019-04-18 17:15:57 -0700

    code change

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 // $(document).ready(function(){});
 //  Question for Omar - can you explain why for some of our excercises $(document).ready(function(){} were at the top
 // but for all these API activities, they are everywhere in the page? 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + keyAPI + "&q=";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=HG3uptoIezihqtSC3MgKA7Aqmb7ZyzBQ";
+// var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + keyAPI + "&q=";
 var keyAPI = "63ba1dba-02a5-4884-9453-68ae543878b6";
 var articleCounter = 0;
 var startYear = 0;
@@ -19,5 +20,4 @@ function startQuery(){
     });
 }
 startQuery();    //getting error in console 401 unauthorized
-
 
